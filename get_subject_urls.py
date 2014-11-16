ed2e954b8229772d304de828477c560e4ac829f5
Evan Krause <evangrayk@gmail.com>
2014-11-15 16:12:36 -0800

    Updated get_subject_urls to actually hit the website instead of using local copy

--- get_subject_urls.py
+++ get_subject_urls.py
@@ -1,14 +1,19 @@
 #!/usr/bin/env python
 
+import urllib2
 import re
 
 #http://www.registrar.ucla.edu/schedule/catsel.aspx
-
 course_regex_s = r'<a href="catalog\.aspx\?sa=([^&]*)&funsel=3">'
 course_regex = re.compile(course_regex_s)
 
-with open('all_subjects.html') as f:
-    s = f.read()
+url = "http://www.registrar.ucla.edu/schedule/catsel.aspx"
+headers = {'User-Agent':"Mozilla/5.0 (X11; U; Linux i686) Gecko/20071127 Firefox/2.0.0.11"}
+
+req = urllib2.Request(url, None, headers)
+response = urllib2.urlopen(req)
+
+s = response.read()
 
 matches = course_regex.findall(s)
 with open('subject_tags.dat', 'w') as f:
